d844bcc107aa2cf1fd02c38e6e7e767948ac3e1e
jakwamano <35068142+jakwamano@users.noreply.github.com>
2022-08-15 22:42:16 +0300

    Create 2.3_6.py
    first test in repo

---
+++ 2.3_6.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import math
+import os
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "https://suninjuly.github.io/alert_accept.html"
+
+browser = webdriver.Chrome()
+browser.get(link)
+
+browser.find_element(By.TAG_NAME, "button").click()
+confirm = browser.switch_to.alert
+confirm.accept()
+
+##browser.switch_to.window(0)
+x_elements = browser.find_element(By.CSS_SELECTOR, "[id='input_value']")
+x = int(x_elements.text)
+y = calc(x)
+browser.find_element(By.ID, "answer").send_keys(y)
+browser.find_element(By.TAG_NAME, "button").click()
+time.sleep(30)
+browser.quit()
